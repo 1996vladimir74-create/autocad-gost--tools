e63fbb71e1379d577cad3e75b6a4d723c2af2465
Владимир <1996vladimir74@gmail.com>
2026-08-07 12:08:02 +0500

    Create app/autocad/document.py

---
+++ app/autocad/document.py
@@ -0,0 +1,41 @@
+import logging
+
+
+class AutoCADDocument:
+
+
+    def __init__(self, acad):
+
+        self.acad = acad
+        self.document = None
+
+        self.logger = logging.getLogger(
+            "AutoCAD_GOST_Tools"
+        )
+
+
+    def create(self):
+
+        self.document = (
+            self.acad.Documents.Add()
+        )
+
+        self.logger.info(
+            "Создан новый DWG документ"
+        )
+
+        return self.document
+
+
+
+    def save(self, path):
+
+        if self.document:
+
+            self.document.SaveAs(
+                path
+            )
+
+            self.logger.info(
+                f"Файл сохранен: {path}"
+            )
