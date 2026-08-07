e613b90f2be2c0e81eac9b8101abcdd3ce6bfe5a
Владимир <1996vladimir74@gmail.com>
2026-08-07 12:00:05 +0500

    Create app/gost/standards.py

---
+++ app/gost/standards.py
@@ -0,0 +1,41 @@
+"""
+Основные параметры ЕСКД
+"""
+
+
+GOST_FRAME = {
+
+    "left_margin": 20,
+    "right_margin": 5,
+    "top_margin": 5,
+    "bottom_margin": 5
+
+}
+
+
+TITLE_BLOCK = {
+
+    "width": 185,
+    "height": 55
+
+}
+
+
+LAYERS = {
+
+    "FRAME": {
+        "color": 7,
+        "lineweight": 0.25
+    },
+
+    "TITLE_BLOCK": {
+        "color": 7,
+        "lineweight": 0.25
+    },
+
+    "TEXT": {
+        "color": 7,
+        "lineweight": 0
+    }
+
+}
