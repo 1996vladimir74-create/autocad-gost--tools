c64c1b195e0960dd1d076c120cb9ed708ed671e0
Владимир <1996vladimir74@gmail.com>
2026-08-08 14:56:06 +0500

    Update geometry.py

--- app/autocad/geometry.py
+++ app/autocad/geometry.py
@@ -1,84 +1,73 @@
-import logging
-
-
 class Geometry:
 
+    def __init__(self, space):
 
-    def __init__(self, document):
+        self.space = space
 
-        self.document = document
+    def line(
+        self,
+        x1,
+        y1,
+        x2,
+        y2,
+        layer=None
+    ):
 
-        self.space = (
-            document.ModelSpace
+        entity = self.space.AddLine(
+            (
+                float(x1),
+                float(y1),
+                0.0
+            ),
+            (
+                float(x2),
+                float(y2),
+                0.0
+            )
         )
 
-        self.logger = logging.getLogger(
-            "AutoCAD_GOST_Tools"
-        )
+        if layer:
+            entity.Layer = layer
 
-
-
-   def line(
-    self,
-    x1,
-    y1,
-    x2,
-    y2,
-    layer=None
-):
-
-    entity = self.space.AddLine(
-        (
-            x1,
-            y1,
-            0
-        ),
-        (
-            x2,
-            y2,
-            0
-        )
-    )
-
-
-    if layer:
-
-        entity.Layer = layer
-
-
-    return entity
-
+        return entity
 
     def rectangle(
         self,
+        x,
+        y,
         width,
-        height
+        height,
+        layer=None
     ):
 
         self.line(
-            0,
-            0,
-            width,
-            0
+            x,
+            y,
+            x + width,
+            y,
+            layer
         )
 
         self.line(
-            width,
-            0,
-            width,
-            height
+            x + width,
+            y,
+            x + width,
+            y + height,
+            layer
         )
 
         self.line(
-            width,
-            height,
-            0,
-            height
+            x + width,
+            y + height,
+            x,
+            y + height,
+            layer
         )
 
         self.line(
-            0,
-            height,
-            0,
-            0
+            x,
+            y + height,
+            x,
+            y,
+            layer
         )
